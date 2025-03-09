998c06fe2bddeb212bac7417d1c491177b9fd172
cristito <cdpsk8@gmail.com>
2025-03-09 17:31:09 -0500

    tag analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 
 const geistSans = Geist({
@@ -28,6 +30,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
+      <GoogleAnalytics gaId="G-16FPV6BY2P" />
     </html>
   );
 }
